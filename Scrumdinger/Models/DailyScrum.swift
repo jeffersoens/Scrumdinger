ffe80072fc36c83a233c455f75f5a8c06374671d
Vyacheslav Gorbatenko <jeffersoens@gmail.com>
2023-04-02 07:51:37 +0300

    Scrumdinger app - Color Set model - Dailty Scrum model - Meeting view first draft

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,36 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Вячеслав Горбатенко on 02.04.2023.
+//
+
+import Foundation
+
+struct DailyScrum {
+    let title: String
+    let attendees: [String]
+    let lengthInMinutes: Int
+    let theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData =
+    [
+        DailyScrum(
+            title: "Design",
+            attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+            lengthInMinutes: 10,
+            theme: .yellow),
+        DailyScrum(
+            title: "App Dev",
+            attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+            lengthInMinutes: 5,
+            theme: .orange),
+        DailyScrum(
+            title: "Web Dev",
+            attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+            lengthInMinutes: 5,
+            theme: .poppy)
+    ]
+}
